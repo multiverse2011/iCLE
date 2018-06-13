bfc9ca1067ed160910e2dcba71c7c78495419a51
multiverse2011 <multiverse2011@gmail.com>
2018-06-13 22:33:31 +0900

    Add settings.bundle

--- cle/ViewController.swift
+++ cle/ViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import WebKit
+class ViewController: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let uid:String = ""
+        let password:String = ""
+        
+        let executeJs:String = "javascript:document.querySelector('#userId').value='" + uid + "';document.querySelector('#password').value='" + password + "';document.querySelector('#loginButton').click();"
+        let userScript = WKUserScript(source: executeJs, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        
+        let controller = WKUserContentController()
+        controller.addUserScript(userScript)
 
-class ViewController: UIViewController {
-
+        let webConfiguration = WKWebViewConfiguration()
+        webConfiguration.userContentController = controller
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let myURL = URL(string:"https://cleweb.tsc.u-tokai.ac.jp")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+        
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        toolbarItems = [refresh]
+        navigationController?.isToolbarHidden = false
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+        if navigationAction.targetFrame == nil {
+            webView.load(navigationAction.request)
+        }
+        return nil
     }
-
-
 }
-
